49ae2a7dfa555a0a2db550e7fcec193d79cc7b76
markbello <mcbello51286@gmail.com>
2019-08-29 22:28:21 -0400

    chore(tests): clean up unused spec files

--- __tests__/components/CarouselImage.spec.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-
-describe('CarouselImage component', () => {
-  it('selects the image in Edit Mode when clicked and not yet selected', () => {
-
-  });
-});
